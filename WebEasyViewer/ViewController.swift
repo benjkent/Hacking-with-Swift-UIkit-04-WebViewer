5906476c88a086088a13e6d500b821173afbf2ab
Benjamin Kent <benjaminfranklinkent@frontier.com>
2021-04-06 13:18:34 -0700

    Add a UIBar at bottom to allow for refresh

--- WebEasyViewer/ViewController.swift
+++ WebEasyViewer/ViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
 
     var webView: WKWebView!
+    var progressView: UIProgressView!
     
     override func loadView() {
         webView = WKWebView()
@@ -22,6 +23,12 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        toolbarItems = [spacer, refresh]
+        navigationController?.isToolbarHidden = false
         
         let url = URL(string: "https://apple.com")!
         webView.load(URLRequest(url: url))
